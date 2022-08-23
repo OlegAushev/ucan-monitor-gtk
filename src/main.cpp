8f27f99f9f86e8ecd594497b1f9aa7dc40b23d57
Oleg Aushev <aushevom@gmail.com>
2022-08-24 00:56:04 +0300

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file main.cpp
+ * @author Oleg Aushev (aushevom@protonmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-08-24
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Hello, GTK!" << std::endl;
+	return 0;
+}
