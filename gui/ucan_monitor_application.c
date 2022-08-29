14bab2604ba8bd843386ef5cce3c50322cb1867f
Oleg Aushev <aushevom@gmail.com>
2022-08-29 21:00:24 +0300

    update about

--- gui/ucan_monitor_application.c
+++ gui/ucan_monitor_application.c
@@ -101,7 +101,7 @@ static void ucan_monitor_application_show_about (GSimpleAction *action,
   window = gtk_application_get_active_window (GTK_APPLICATION (self));
 
   gtk_show_about_dialog (window,
-                         "program-name", "uCAN-monitor",
+                         "program-name", "uCAN Monitor",
                          "authors", authors,
                          "version", "0.1.0",
                          NULL);
