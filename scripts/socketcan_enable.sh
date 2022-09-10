aa37b20a8146df1cc25165952446d4ce9baf129c
Oleg Aushev <aushevom@gmail.com>
2022-09-10 12:37:26 +0300

    add cansocket script, update cmakelists

---
+++ scripts/socketcan_enable.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+if [ -z $1 ] || [ -z $2 ]
+then
+	echo "Invalid arguments."
+	exit 1
+fi
+if ip addr ls dev $1 > /dev/null 2>&1
+then
+	echo "Device $1 found."
+	if ip link set $1 down
+	then
+		ip link set $1 type can bitrate $2
+		ip link set $1 up
+		echo "SocketCAN interface $1 (bitrate = $2) enabled."
+		exit 0
+	else
+		echo "Configuring and enabling SocketCAN interface $1 failed."
+		exit 3
+	fi
+else
+	echo "Device $1 not found."
+	exit 2
+fi
