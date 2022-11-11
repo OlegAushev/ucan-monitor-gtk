3ce352fbf345571771f72b2467d27c0a4a617b87
Oleg Aushev <aushevom@gmail.com>
2022-11-11 14:46:37 +0300

    add abstract server node

--- src/ucanopen/ucanopen_servernode/ucanopen_servernode.h
+++ src/ucanopen/ucanopen_servernode/ucanopen_servernode.h
@@ -112,6 +112,109 @@ public:
 };
 
 
+
+
+
+
+
+class IServerNode
+{
+public:
+	NodeId nodeId;
+private:
+	std::shared_ptr<can::Socket> m_socket;
+private:
+	/* OBJECT DICTIONARY */
+	const ObjectDictionaryType& m_dictionary;
+	ObjectDictionaryAuxType m_dictionaryAux;
+
+	/* TPDO server --> client */
+private:
+	struct TpdoInfo
+	{
+		std::chrono::time_point<std::chrono::steady_clock> timepoint;
+	};
+	std::map<TpdoType, TpdoInfo> m_tpdoInfo;
+protected:
+	virtual void processTpdo1(std::array<uint8_t, 8> data) = 0;
+	virtual void processTpdo2(std::array<uint8_t, 8> data) = 0;
+	virtual void processTpdo3(std::array<uint8_t, 8> data) = 0;
+	virtual void processTpdo4(std::array<uint8_t, 8> data) = 0;
+
+	/* RPDO server <-- client */
+private:
+	bool m_isRpdoEnabled;
+	struct RpdoInfo
+	{
+		std::chrono::milliseconds period;
+		std::chrono::time_point<std::chrono::steady_clock> timepoint;
+	};
+	std::map<RpdoType, RpdoInfo> m_rpdoInfo;
+protected:
+	virtual std::array<uint8_t, 8> makeRpdo1() = 0;
+	virtual std::array<uint8_t, 8> makeRpdo2() = 0;
+	virtual std::array<uint8_t, 8> makeRpdo3() = 0;
+	virtual std::array<uint8_t, 8> makeRpdo4() = 0;
+
+	/* TSDO server --> client */
+protected:
+	virtual void processTsdo(SdoType, ObjectDictionaryType::const_iterator, CobSdoData) = 0;
+
+public:
+	void enableRpdo() { m_isRpdoEnabled = true; }
+	void disableRpdo() { m_isRpdoEnabled = false; }
+	void sendRpdo();
+
+	ODRequestStatus read(std::string_view category, std::string_view subcategory, std::string_view name);
+	ODRequestStatus write(std::string_view category, std::string_view subcategory, std::string_view name, CobSdoData data);
+	ODRequestStatus write(std::string_view category, std::string_view subcategory, std::string_view name, std::string value);
+	ODRequestStatus exec(std::string_view category, std::string_view subcategory, std::string_view name);
+
+	std::vector<std::string_view> watchEntriesList() const;
+
+private:
+	void onFrameReceived(can_frame frame);
+	
+	std::map<ODEntryKey, ODEntryValue>::const_iterator findOdEntry(
+		std::string_view category,
+		std::string_view subcategory,
+		std::string_view name)
+	{
+		auto it = m_dictionaryAux.find({category, subcategory, name});
+		if (it == m_dictionaryAux.end())
+		{
+			return m_dictionary.end();
+		}
+		return it->second;
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 } // namespace ucanopen
 
 
