e26351bc6c8f02dfe2d147d0042c71325e2804a0
Oleg Aushev <aushevom@gmail.com>
2022-11-14 14:49:05 +0300

    add plain can template

---
+++ src/plaincan/controller/plaincan_controller.h
@@ -0,0 +1,15 @@
+/**
+ * @file plaincan_controller.h
+ * @author Oleg Aushev (aushevom@protonmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-14
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+#pragma once
+
+
---
+++ src/plaincan/device/plaincan_device.h
@@ -0,0 +1,15 @@
+/**
+ * @file plaincan_device.h
+ * @author Oleg Aushev (aushevom@protonmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-14
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+#include "plaincan_device.h"
+
+
