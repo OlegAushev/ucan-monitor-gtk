07dd52379a1e6ffa06bdb7cce46b7b733ee9f7c9
Oleg Aushev <aushevom@gmail.com>
2022-11-20 18:26:54 +0300

    add vcm message 0x1D4

--- src/atv/vcm/vcm.h
+++ src/atv/vcm/vcm.h
@@ -22,14 +22,27 @@ private:
 	struct Message0x1D4
 	{
 		uint8_t byte0_reserved : 8;
+
 		uint8_t byte1_reserved : 8;
-		uint8_t torqueRequestM : 8;
-		uint16_t byte3_reserved : 4;
-		uint8_t torqueRequestL : 4;
-	};
 		
+		uint8_t torqueRequestM : 8;
 
+		uint8_t byte3_reserved : 4;
+		uint8_t torqueRequestL : 4;
 
+		uint8_t byte4_reserved0 : 2;
+		uint8_t statusHvPowerSupply : 1;
+		uint8_t byte4_reserved1 : 3;
+		uint8_t vcmClock : 2;
+
+		uint8_t byte5_reserved0 : 6;
+		uint8_t statusRelayPlusOutput : 1;
+		uint8_t byte5_reserved1 : 1;
+
+		uint8_t statusCharge : 8;
+
+		uint8_t crc : 8;
+	};
 
 public:
 
