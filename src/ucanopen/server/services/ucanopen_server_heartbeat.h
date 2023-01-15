324783af48a88c18e97f0fbe3b349244a37041fe
Oleg Aushev <aushevom@gmail.com>
2023-01-15 23:08:10 +0300

    refactor

--- src/ucanopen/server/services/ucanopen_server_heartbeat.h
+++ src/ucanopen/server/services/ucanopen_server_heartbeat.h
@@ -34,7 +34,7 @@ public:
 	bool isOk() const
 	{
 		return ((std::chrono::steady_clock::now() - _timepoint) <= _timeout)
-				&& (_server->_nmtState == NmtState::Operational);
+				&& (_server->nmtState() == NmtState::Operational);
 	}
 
 	void updateNodeId();
