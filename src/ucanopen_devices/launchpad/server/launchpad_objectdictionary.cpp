bb60f571b89342bbaef510d628c853341945ad3b
Oleg Aushev <aushevom@gmail.com>
2023-02-11 00:48:59 +0300

    update OD

--- src/ucanopen_devices/launchpad/server/launchpad_objectdictionary.cpp
+++ src/ucanopen_devices/launchpad/server/launchpad_objectdictionary.cpp
@@ -18,8 +18,8 @@ extern const ucanopen::ObjectDictionary object_dictionary = {
 {{0x5FFF, 0x00}, {"system", "info", "firmware_version", "", OD_STRING_4CHARS, OD_ACCESS_RO}},
 {{0x5FFF, 0x01}, {"system", "info", "build_configuration", "", OD_STRING_4CHARS, OD_ACCESS_RO}},
 
-{{0x2000, 0x00}, {"system", "device", "reset_device", "", OD_EXEC, OD_ACCESS_RO}},
-{{0x2000, 0x01}, {"system", "syslog", "reset_errors", "", OD_EXEC, OD_ACCESS_RO}},
+{{0x2000, 0x00}, {"system", "device", "reset_device", "", OD_EXEC, OD_ACCESS_WO}},
+{{0x2000, 0x01}, {"system", "syslog", "reset_errors", "", OD_EXEC, OD_ACCESS_WO}},
 
 {{0x5000, 0x00}, {"watch", "watch", "uptime", "s", OD_FLOAT32, OD_ACCESS_RO}},
 {{0x5000, 0x01}, {"watch", "watch", "syslog_message", "", OD_UINT32, OD_ACCESS_RO}},
