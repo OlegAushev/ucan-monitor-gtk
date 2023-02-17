610c6b3a74e07b7a26f77593dcd705eb40be71a9
Oleg Aushev <aushevom@gmail.com>
2023-02-17 10:19:03 +0300

    refactor

---
+++ src/ucanopen/server/utils/ucanopen_server_od_utils.cpp
@@ -0,0 +1,74 @@
+#include "ucanopen_server_od_utils.h"
+
+
+namespace ucanopen {
+
+namespace utils {
+
+
+uint32_t SerialNumberGetter::get(std::future<void> signal_terminate) const
+	{
+		_server->read("sys", "info", "serial_number");
+		while (signal_terminate.wait_for(std::chrono::milliseconds(0)) == std::future_status::timeout
+				&& _serial_number == 0)
+		{/*WAIT*/}
+		return _serial_number;
+	}
+
+int SerialNumberGetter::handle_sdo(SdoType sdo_type, ObjectDictionaryEntries::const_iterator entry_iter, ExpeditedSdoData sdo_data)
+{
+	if (sdo_type == SdoType::response_to_read
+		&& entry_iter->second.category == "sys"
+		&& entry_iter->second.subcategory == "info"
+		&& entry_iter->second.name == "serial_number")
+	{
+		_serial_number = sdo_data.u32();
+		return 0;
+	}
+	return 1;
+}
+
+
+std::string DeviceNameGetter::get(std::future<void> signal_terminate) const
+{
+	_server->read("sys", "info", "device_name");
+	while (signal_terminate.wait_for(std::chrono::milliseconds(0)) == std::future_status::timeout
+			&& !_ready)
+	{/*WAIT*/}
+	return _device_name;
+}
+
+int DeviceNameGetter::handle_sdo(SdoType sdo_type, ObjectDictionaryEntries::const_iterator entry_iter, ExpeditedSdoData sdo_data)
+{
+	if (sdo_type == SdoType::response_to_read
+		&& entry_iter->second.category == "sys"
+		&& entry_iter->second.subcategory == "info"
+		&& entry_iter->second.name == "device_name")
+	{
+		std::array<char, 4> char_arr;
+		memcpy(char_arr.data(), &sdo_data, 4);
+		for (auto ch : char_arr)
+		{
+			_char_vec.push_back(ch);
+			if (ch == '\0')
+			{
+				_device_name = std::string(_char_vec.data());
+				_ready = true;
+				break;
+			}
+		}
+
+		if (!_ready)
+		{
+			_server->read("sys", "info", "device_name");
+		}
+
+		return 0;
+	}
+	return 1;
+}
+
+} // namespace utils
+
+} // namespace ucanopen
+
--- src/ucanopen/server/utils/ucanopen_server_od_utils.h
+++ src/ucanopen/server/utils/ucanopen_server_od_utils.h
@@ -2,6 +2,7 @@
 
 
 #include "../impl/ucanopen_impl_server.h"
+#include <vector>
 #include <future>
 
 
@@ -19,28 +20,8 @@ public:
 		: impl::SdoSubscriber(publisher)
 		, _server(server)
 	{}
-	
-	uint32_t get(std::future<void> signal_terminate) const
-	{
-		_server->read("sys", "info", "serial_number");
-		while (signal_terminate.wait_for(std::chrono::milliseconds(0)) == std::future_status::timeout
-				&& _serial_number == 0)
-		{/*WAIT*/}
-		return _serial_number;
-	}
-
-	virtual int handle_sdo(SdoType sdo_type, ObjectDictionaryEntries::const_iterator entry_iter, ExpeditedSdoData sdo_data)
-	{
-		if (sdo_type == SdoType::response_to_read
-			&& entry_iter->second.category == "sys"
-			&& entry_iter->second.subcategory == "info"
-			&& entry_iter->second.name == "serial_number")
-		{
-			_serial_number = sdo_data.u32();
-			return 0;
-		}
-		return 1;
-	}
+	uint32_t get(std::future<void> signal_terminate) const;
+	virtual int handle_sdo(SdoType sdo_type, ObjectDictionaryEntries::const_iterator entry_iter, ExpeditedSdoData sdo_data);
 };
 
 
@@ -55,46 +36,9 @@ public:
 	DeviceNameGetter(impl::Server* server, impl::SdoPublisher* publisher)
 		: impl::SdoSubscriber(publisher)
 		, _server(server)
-	{}
-	
-	std::string get(std::future<void> signal_terminate) const
-	{
-		_server->read("sys", "info", "device_name");
-		while (signal_terminate.wait_for(std::chrono::milliseconds(0)) == std::future_status::timeout
-				&& !_ready)
-		{/*WAIT*/}
-		return _device_name;
-	}
-
-	virtual int handle_sdo(SdoType sdo_type, ObjectDictionaryEntries::const_iterator entry_iter, ExpeditedSdoData sdo_data)
-	{
-		if (sdo_type == SdoType::response_to_read
-			&& entry_iter->second.category == "sys"
-			&& entry_iter->second.subcategory == "info"
-			&& entry_iter->second.name == "device_name")
-		{
-			std::array<char, 4> char_arr;
-			memcpy(char_arr.data(), &sdo_data, 4);
-			for (auto ch : char_arr)
-			{
-				_char_vec.push_back(ch);
-				if (ch == '\0')
-				{
-					_device_name = std::string(_char_vec.data());
-					_ready = true;
-					break;
-				}
-			}
-
-			if (!_ready)
-			{
-				_server->read("sys", "info", "device_name");
-			}
-
-			return 0;
-		}
-		return 1;
-	}
+	{}	
+	std::string get(std::future<void> signal_terminate) const;
+	virtual int handle_sdo(SdoType sdo_type, ObjectDictionaryEntries::const_iterator entry_iter, ExpeditedSdoData sdo_data);
 };
 
 }
